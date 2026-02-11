c857d0ad7237d50f8fb2b972f21bc908f444970b
Benjamin Cepres <cepresbenj@gmail.com>
2026-02-11 20:40:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const card = document.getElementById("card");
+const success = document.getElementById("success");
+
+let yesScale = 1;
+
+noBtn.addEventListener("mouseenter", () => {
+    const cardRect = card.getBoundingClientRect();
+    const btnRect = noBtn.getBoundingClientRect();
+
+    const maxX = cardRect.width - btnRect.width - 20;
+    const maxY = cardRect.height - btnRect.height - 20;
+
+    const x = Math.random() * maxX;
+    const y = Math.random() * maxY;
+
+    noBtn.style.position = "absolute";
+    noBtn.style.left = x + "px";
+    noBtn.style.top = y + "px";
+
+    yesScale += 0.15;
+    yesBtn.style.transform = `scale(${yesScale})`;
+});
+
+yesBtn.addEventListener("click", () => {
+    card.classList.add("hidden");
+    success.classList.remove("hidden");
+});
